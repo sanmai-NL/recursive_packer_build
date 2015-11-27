48a3e42f5da3b789c2551b62804594fddfb259d6
Sander M <S.N.Maijers@gmail.com>
2015-11-27 18:48:23 +0100

    * FIX: remove unhelpful set -e. * ENHANCE: add informative -x parameter to shebang line.

--- recursive_packer_build.sh
+++ recursive_packer_build.sh
@@ -1,12 +1,10 @@
-#!/bin/sh
+#!/bin/sh -x
 
 build_all() {
-    set -e
     for packer_file in $(find . $(git submodule --quiet foreach --recursive 'pwd') -maxdepth '1' -type 'f' -name 'packer.json' | tac)
     do
         set -x
         packer build "${packer_file}"
         set +x
     done
-    set +e
 }
